7c5e7eef92dcf6a4b334f5c81fc4bd28c1896630
ShawnHsu <shianhian327@gmail.com>
2023-06-08 18:39:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # OPENAI API Key初始化設定
+print("OPENAI_API_KEY", os.getenv('OPENAI_API_KEY'))
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
