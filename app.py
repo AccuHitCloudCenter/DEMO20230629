2cc1789a54a2d0b27d7d98f96c6be5aa0a69204a
shianshiu <shianhian327@gmail.com>
2023-06-13 21:27:04 +0800

    add open api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Author: Shawn
 Date: 2023-06-13 20:14:08
 LastEditors: Shawn
-LastEditTime: 2023-06-13 20:22:44
+LastEditTime: 2023-06-13 21:25:05
 FilePath: /CloudArchitectures/linebot_openai/app.py
 Description: 
 
@@ -14,7 +14,7 @@ from linebot.exceptions import InvalidSignatureError
 from flask import Flask, request, abort
 from dotenv import load_dotenv
 import os
-
+import openai
 # 加载 .env 文件中的环境变量
 load_dotenv()
 
@@ -28,6 +28,28 @@ handler = WebhookHandler(channel_secret)
 
 app = Flask(__name__)
 
+def call_openai(text):
+    openai.api_type = "azure"
+    openai.api_base = "https://product-cdp-open-ai-dev-01.openai.azure.com/"
+    openai.api_version = "2023-03-15-preview"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    response = openai.ChatCompletion.create(
+    engine="gpt-35-turbo",
+    messages = [{"role":"system","content":"You are an AI assistant that helps people find information."},
+                {"role":"user","content":text}],
+    temperature=0.7,
+    max_tokens=800,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None)
+
+    return response['choices'][0]['message']['content']
+
+
+
+
 # 处理 Line 的 Webhook 请求
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -40,12 +62,15 @@ def callback():
         abort(400)
     return "OK"
 
+
 # 处理文本消息的函数
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text
-    reply_text = "你发送了：{}".format(text)
+    # reply_text = "你发送了：{}".format(text)
+    reply_text = call_openai(text)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+
 
 if __name__ == "__main__":
     app.run()
