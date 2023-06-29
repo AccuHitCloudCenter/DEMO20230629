26d99153efd6b1473221b2d5ae2081b6741795de
AccuHitCloudCenter <137851815+AccuHitCloudCenter@users.noreply.github.com>
2023-06-29 21:14:49 +0800

    Update utils_openai.py

--- modules/utils_openai.py
+++ modules/utils_openai.py
@@ -19,13 +19,13 @@ async def call_openai(question: str):
 
 def call_openai_sync(text):
     openai.api_type = "azure"
-    openai.api_base = "https://product-cdp-open-ai-dev-01.openai.azure.com/"
+    openai.api_base = "https://aoia-demo2.openai.azure.com/"
     openai.api_version = "2023-03-15-preview"
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
     response = openai.ChatCompletion.create(
-    engine="gpt-35-turbo",
-    messages = [{"role":"system","content":"你的名字是Iris，身分是東海大學的智能客服，問答的內容僅涉及學校相關的事務，回答的內容要活潑、適當使用各種顏文字，但回答不要過長，回答盡量快速."},
+    engine="TEAMS",
+    messages = [{"role":"system","content":"名字是Iris，身分是東海大學的智能客服，問答的內容僅涉及學校相關的事務，回答的內容要活潑、適當使用各種顏文字，但回答不要過長，回答盡量快速"},  #You are an AI assistant that helps people find information
                 {"role":"user","content":text}],
     temperature=0.7,
     max_tokens=800,
