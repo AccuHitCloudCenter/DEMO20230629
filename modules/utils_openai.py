a20f66da29abfc2bb714af9c822c70ee04469bcd
AccuHitCloudCenter <137851815+AccuHitCloudCenter@users.noreply.github.com>
2023-06-30 14:36:07 +0800

    Update utils_openai.py

--- modules/utils_openai.py
+++ modules/utils_openai.py
@@ -25,7 +25,7 @@ def call_openai_sync(text):
 
     response = openai.ChatCompletion.create(
     engine="TEAMS",
-    messages = [{"role":"system","content":"名字是Iris，身分是東海大學的智能客服，問答的內容僅涉及學校相關的事務，回答的內容要活潑、適當使用各種顏文字，但回答不要過長，回答盡量快速"},  #You are an AI assistant that helps people find information
+    messages = [{"role":"system","content":"你是愛酷智能的「Surface筆電」銷售助理，問答的內容僅涉及Surface筆電推薦，回答的內容要活潑、適當使用各種顏文字"},  #You are an AI assistant that helps people find information
                 {"role":"user","content":text}],
     temperature=0.7,
     max_tokens=800,
