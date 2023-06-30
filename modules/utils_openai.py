d87ceeb01c97e984a297c1782f2ef14916fcf30d
AccuHitCloudCenter <137851815+AccuHitCloudCenter@users.noreply.github.com>
2023-06-30 14:30:59 +0800

    Update utils_openai.py

--- modules/utils_openai.py
+++ modules/utils_openai.py
@@ -1,8 +1,16 @@
+'''
+Author: Shawn
+Date: 2023-06-16 10:52:51
+LastEditors: Shawn
+LastEditTime: 2023-06-16 13:55:57
+FilePath: /CloudArchitectures/linebot_openai/modules/utils_openai.py
+Description: 
+
+Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+'''
 import asyncio
 import openai
-import os
-
-conversation_history = []
+import os 
 
 async def call_openai(question: str):
     loop = asyncio.get_event_loop()
@@ -15,19 +23,15 @@ def call_openai_sync(text):
     openai.api_version = "2023-03-15-preview"
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    conversation_history.append({"role":"user","content":text}) 
-
     response = openai.ChatCompletion.create(
     engine="TEAMS",
-    messages = [{"role":"system","content":"你的身分是愛酷智能的Surface銷售助手，問答的內容僅涉Surface銷售相關，回答的內容要活潑、適當使用各種顏文字，但回答不要過長"},
-                {"role":"system","content":"只銷售Surface Go 3、Surface Pro 9、Surface Laptop Studio、Surface Laptop Go 2、Surface Laptop 5"}] + conversation_history,
+    messages = [{"role":"system","content":"名字是Iris，身分是東海大學的智能客服，問答的內容僅涉及學校相關的事務，回答的內容要活潑、適當使用各種顏文字，但回答不要過長，回答盡量快速"},  #You are an AI assistant that helps people find information
+                {"role":"user","content":text}],
     temperature=0.7,
     max_tokens=800,
     top_p=0.95,
     frequency_penalty=0,
     presence_penalty=0,
     stop=None)
-
-    conversation_history.append({"role":"assistant","content":response['choices'][0]['message']['content']})
 
     return response['choices'][0]['message']['content']
